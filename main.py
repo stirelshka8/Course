bde44fc727ace944fb7e388eedac61edf2231ace
stirelshka8 <stirelshka8@gmail.com>
2022-08-04 20:50:20 +0300

    Добавлена фукция получения json по фотографиям

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+import requests
 from configuration import access_token
 
-print(access_token)
+
+class VkPhoto:
+
+    def __init__ (self, token_vk):
+        self.token = token_vk
+
+    def data_photos (self,offset=0, count=50):
+            url = 'https://api.vk.com/method/photos.get'
+            params = {'owner_id': '1',
+                      'album_id': 'profile',
+                      'access_token': self.token,
+                      'v': '5.131',
+                      'extended': '1',
+                      'photo_sizes': '1',
+                      'count': count,
+                      'offset': offset
+                      }
+
+            request_vk = requests.get(url=url, params=params)
+            return request_vk.json()
+
+asdasd = VkPhoto(access_token)
+print(asdasd.data_photos())
