e4fc1a7d48ee96c6244a46d04e4f9f0d1705da3c
stirelshka8 <stirelshka8@gmail.com>
2022-08-05 19:56:54 +0300

    В разработке функция извлечения фотграфий

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from configuration import access_token
 
@@ -7,20 +8,38 @@ class VkPhoto:
     def __init__ (self, token_vk):
         self.token = token_vk
 
-    def data_photos (self,offset=0, count=50):
-            url = 'https://api.vk.com/method/photos.get'
-            params = {'owner_id': '1',
-                      'album_id': 'profile',
-                      'access_token': self.token,
-                      'v': '5.131',
-                      'extended': '1',
-                      'photo_sizes': '1',
-                      'count': count,
-                      'offset': offset
-                      }
+    def _data_photos (self,offset=0, count=50):
+        url = 'https://api.vk.com/method/photos.get'
+        params = {'owner_id': '1',
+                    'album_id': 'profile',
+                    'access_token': self.token,
+                    'v': '5.131',
+                    'extended': '1',
+                    'photo_sizes': '1',
+                    'count': count,
+                    'offset': offset
+                    }
 
-            request_vk = requests.get(url=url, params=params)
-            return request_vk.json()
+        request_vk = requests.get(url=url, params=params)
+        return request_vk.json()
 
-asdasd = VkPhoto(access_token)
-print(asdasd.data_photos())
+    def extracting_photos (self):
+        if not os.path.exists('photo'):
+            os.mkdir('photo')
+
+        self.extracting_data = self._data_photos()
+        self.number_all_photos = self.extracting_data['response']['count']
+        self.list_photo = []
+        self.name_and_link = {}
+        self.step = 0
+        self.count = 50
+
+        while self.step <= self.number_all_photos:
+            if self.step != 0:
+                self.extracting_data = self._data_photos(offset=self.step, count=self.count)
+
+
+
+aaaa = VkPhoto(access_token)
+print(aaaa._data_photos())
+
