1a808a0751b7e2b99ac7fb244d79b89ea9aadd08
stirelshka8 <stirelshka8@gmail.com>
2022-08-07 21:25:09 +0300

    Функция загружает фотографии

--- main.py
+++ main.py
@@ -54,8 +54,19 @@ class VkPhoto:
                     self.name_and_link[f"{self.extracting_photo['likes']['count']} + {self.extracting_photo['date']}"] = self.size_photo['url']
                     self.info_extracting_photo['file_name'] = f"{self.extracting_photo['likes']['count']} + {self.extracting_photo['date']}.jpg"
 
-            print(self.name_and_link)
-            self.step += 1
+                self.info_extracting_photo['size'] = self.size_photo['type']
+                self.list_photo.append(self.info_extracting_photo)
+
+            for self.name_and_link_key, self.name_and_link_val in self.name_and_link.items():
+                with open('photo/%s' % f'{self.name_and_link_key}.jpg', 'wb') as self.open_file:
+                    self.images_open = requests.get(self.name_and_link_val)
+                    self.open_file.write(self.images_open.content)
+            
+            with open('info_photo.json', 'w') as self.open_json:
+                json.dump(self.list_photo, self.open_json, indent=4)
+            
+            self.step += self.count
+
 
 aaaa = VkPhoto(access_token)
 print(aaaa.extracting_photos())
