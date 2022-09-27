31fcffdc235a4ee1b1d52dec2ba082a1ed36ce37
stirelshka8 <stirelshka8@gmail.com>
2022-09-27 17:41:29 +0300

    pass

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from datetime import date
 
 logging.basicConfig(filename="logging.log", level=logging.INFO)
 
+
 def startup():
     os.system('clear')
     input_user_id = str(input("Введите ID пользователя ВКонтакте - > "))
     input_yandex_token = str(input("Введите токен Я.Диска для загрузки фотографий - > "))
-    #input_yandex_token = access_token_YA
+    # input_yandex_token = access_token_YA
     name_temp_folder = f'{folder_name}_{date.today()}'
-    
+
     if not os.path.exists(name_temp_folder):
         os.mkdir(name_temp_folder)
         print(f"[INFO] Директория для загрузки фотографий /{name_temp_folder}/ создана")
@@ -29,10 +30,10 @@ def startup():
         dir_photos = os.listdir(name_temp_folder)
         photo_counter = 0
         print('[INFO] Начало загрузки фотографий на Я.Диск')
-        bar_upload = IncrementalBar('[INFO] Загрузка', max = len(dir_photos))
+        bar_upload = IncrementalBar('[INFO] Загрузка', max=len(dir_photos))
         for dir_photo in dir_photos:
-            bar_upload.next() 
-            file_photo_name = dir_photo      
+            bar_upload.next()
+            file_photo_name = dir_photo
             file_path = f'{os.getcwd()}/{name_temp_folder}/{dir_photo}'
 
             try:
@@ -40,40 +41,51 @@ def startup():
             except requests.exceptions.MissingSchema:
                 os.system('clear')
                 print('[ERROR] Ошибка загрузки фотографий. Возможно введен не верный токен Я.Диска!')
-                logging.error(f"{datetime.datetime.now()} Ошибка загрузки фотографий. Возможно введен не верный токен Я.Диска!")
+                logging.error(
+                    f"{datetime.datetime.now()} Ошибка загрузки фотографий. Возможно введен не верный токен Я.Диска!")
                 break
 
             photo_counter += 1
-        bar_upload.finish()    
+        bar_upload.finish()
         print(f'[INFO] Загружено {photo_counter} фотографий на Я.Диск')
-        logging.info(f"{datetime.datetime.now()} На Я.Диск, в папку \{getpass.getuser()}\ загружено {photo_counter} фотографий")
+        logging.info(
+            f"{datetime.datetime.now()} На Я.Диск, в папку \{getpass.getuser()}\ загружено {photo_counter} фотографий")
     except KeyError:
         print('[ERROR] Ошибка загрузки фотографий. Возможно не введен токен ВКонтакте!')
-        logging.error(f"{datetime.datetime.now()} Ошибка загрузки фотографий. Возможно не введен токен ВКонтакте в файле конфигурации!")
-  
+        logging.error(
+            f"{datetime.datetime.now()} Ошибка загрузки фотографий. Возможно не введен токен ВКонтакте в файле конфигурации!")
+
 
 class VkPhoto:
-    def __init__ (self, token_vk, user_id, name_temp_folder):
+    def __init__(self, token_vk, user_id, name_temp_folder):
+        self.extracting_photo = None
+        self.counter_download_photo = None
+        self.count = None
+        self.step = None
+        self.name_and_link = None
+        self.list_photo = None
+        self.number_all_photos = None
+        self.extracting_data = None
         self.name_temp_folder = name_temp_folder
         self.token = token_vk
         self.user_id = user_id
 
-    def _data_photos (self, offset=0, count=50):
+    def _data_photos(self, offset=0, count=50):
         url = 'https://api.vk.com/method/photos.get'
         params = {'owner_id': self.user_id,
-                    'album_id': 'profile',
-                    'access_token': self.token,
-                    'v': '5.131',
-                    'extended': '1',
-                    'photo_sizes': '1',
-                    'count': count,
-                    'offset': offset
-                    }
+                  'album_id': 'profile',
+                  'access_token': self.token,
+                  'v': '5.131',
+                  'extended': '1',
+                  'photo_sizes': '1',
+                  'count': count,
+                  'offset': offset
+                  }
 
         request_vk = requests.get(url=url, params=params)
         return request_vk.json()
 
-    def extracting_photos (self):
+    def extracting_photos(self):
         self.extracting_data = self._data_photos()
         self.number_all_photos = self.extracting_data['response']['count']
         self.list_photo = []
@@ -89,7 +101,7 @@ class VkPhoto:
             for self.extracting_photo in self.extracting_data['response']['items']:
                 self.size_extracting_photo = 0
                 self.info_extracting_photo = {}
-                
+
                 for self.size_photo in self.extracting_photo['sizes']:
                     if self.size_photo['height'] >= self.size_extracting_photo:
                         self.size_extracting_photo = self.size_photo['height']
@@ -98,37 +110,41 @@ class VkPhoto:
                     self.name_and_link[self.extracting_photo['likes']['count']] = self.size_photo['url']
                     self.info_extracting_photo['file_name'] = f"{self.extracting_photo['likes']['count']}.jpg"
                 else:
-                    self.name_and_link[f"{self.extracting_photo['likes']['count']}_{self.extracting_photo['date']}"] = self.size_photo['url']
-                    self.info_extracting_photo['file_name'] = f"{self.extracting_photo['likes']['count']}_{self.extracting_photo['date']}.jpg"
+                    self.name_and_link[f"{self.extracting_photo['likes']['count']}_{self.extracting_photo['date']}"] = \
+                    self.size_photo['url']
+                    self.info_extracting_photo[
+                        'file_name'] = f"{self.extracting_photo['likes']['count']}_{self.extracting_photo['date']}.jpg"
 
                 self.info_extracting_photo['size'] = self.size_photo['type']
                 self.list_photo.append(self.info_extracting_photo)
             print("[INFO] Начало загрузки фотографий из профиля ВК")
-            bar_download = IncrementalBar('[INFO] Загрузка', max = len(self.name_and_link.items()))
+            bar_download = IncrementalBar('[INFO] Загрузка', max=len(self.name_and_link.items()))
             for self.name_and_link_key, self.name_and_link_val in self.name_and_link.items():
                 with open('photo/%s' % f'{self.name_and_link_key}.jpg', 'wb') as self.open_file:
                     self.images_open = requests.get(self.name_and_link_val)
                     self.open_file.write(self.images_open.content)
                     self.counter_download_photo += 1
                 bar_download.next()
-            bar_download.finish()  
+            bar_download.finish()
             print(f'[INFO] Загружено {self.counter_download_photo} фотографий с профиля ВК')
-            logging.info(f"{datetime.datetime.now()} Из профиля Вконтакте ID - {self.user_id}, в папку \{self.name_temp_folder}\ загружено {self.counter_download_photo} фотографий")
+            logging.info(
+                f"{datetime.datetime.now()} Из профиля Вконтакте ID - {self.user_id}, в папку \{self.name_temp_folder}\ загружено {self.counter_download_photo} фотографий")
             with open('info_photo.json', 'w') as self.open_json:
                 json.dump(self.list_photo, self.open_json, indent=4)
             self.step += self.count
 
+
 class YandexUpload:
     def __init__(self, yandex_token):
-        self.yandex_token = yandex_token       
+        self.yandex_token = yandex_token
         self.name_folder = f'{getpass.getuser()}_{date.today()}'
-            
+
     def creating_directory(self):
         self.yandex_url = 'https://cloud-api.yandex.net/v1/disk/resources/'
         self.headers = {'Content-Type': 'application/json', 'Authorization': f'OAuth {self.yandex_token}'}
         self.params = {'path': f'{self.name_folder}', 'overwrite': 'false'}
         self.send_request = requests.put(url=self.yandex_url, headers=self.headers, params=self.params)
-    
+
     def upload_photo(self, file_path, file_photo_name):
         self.file_path = file_path
         self.startup_list = file_photo_name
@@ -140,6 +156,7 @@ class YandexUpload:
         self.received_link = self.request_yandex.json().get('href')
         self.uploader_photo = requests.put(self.received_link, data=open(self.file_path, 'rb'))
         return
+
 
 if __name__ == '__main__':
     startup()
